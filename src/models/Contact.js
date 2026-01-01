8c41e054f590e95b5f83c77dd196d4e450283447
luc-create <154468627+luc-create@users.noreply.github.com>
2026-01-01 18:34:24 +0000

    mis a jour du controller reservation2

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -5,6 +5,7 @@ const contactSchema = new mongoose.Schema(
     nom: { type: String, required: true, trim: true },
     email: { type: String, required: true, lowercase: true, trim: true },
     telephone: { type: String, required: false, trim: true },
+    sujet: { type: String, required: true, trim: true },
     message: { type: String, required: true, trim: true },
     date: { type: Date, default: Date.now },
     lu: { type: Boolean, default: false },
--- src/validators/contacts.validators.js
+++ src/validators/contacts.validators.js
@@ -4,7 +4,8 @@ const contactCreateSchema = Joi.object({
   body: Joi.object({
     nom: Joi.string().required(),
     email: Joi.string().email().required(),
-    telephone: Joi.string().allow('', null),
+    telephone: Joi.string().allow('', null).optional(),
+    sujet: Joi.string().required(),
     message: Joi.string().min(5).required(),
   }),
   params: Joi.object({}).unknown(true),
